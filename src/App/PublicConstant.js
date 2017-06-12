5ee8b8210c83ba68f2d5e06ea69b040be92ba322
姚陈堃 <ken@macbook.local>
2017-06-12 21:30:12 +0800

    change port

--- src/App/PublicConstant.js
+++ src/App/PublicConstant.js
@@ -1,6 +1,6 @@
-//const SERVER = 'http://10.109.247.127:8080' //lab
+const SERVER = 'http://10.109.247.127:8080' //lab
 //const SERVER = 'http://10.108.113.8:8080' //renzilong_lab
-const SERVER = 'http://localhost:8080' //yaochenkun_mac
+//const SERVER = 'http://localhost:8080' //yaochenkun_mac
 const LOADING_DELAY_TIME = 500 //加载延迟时间，若在0.5s内加载完毕则不显示
 
 
